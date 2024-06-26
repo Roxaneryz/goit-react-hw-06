1b9ef70543d43ee1c491929ec1dfc868e34fc0fa
Roxaneryz <roxane5666@yahoo.gr>
2024-06-26 20:08:24 +0300

    inisialstate

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,22 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-const initialState = {
-    items: []
+const initialContactsState = {
+    items: [
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ]
 };
 
-const contactSlice = createSlice({
 
-    name: "contact",
-    initialState,
-    reducers: {
-        addContact: (state, action) => {
-            state.items.push(action.payload);
-        },
-        deleteContact: (state, action) => {
-            state.items = state.items.filter(contact => contact.id !== action.payload);
-        }
-    }
+
+const contactSlice = createSlice({
+  name: "contact",
+  initialContactsState,
+  reducers: {
+    addContact: (state, action) => {
+      state.items.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      state.items = state.items.filter(
+        (contact) => contact.id !== action.payload
+      );
+    },
+  },
 });
 
 
@@ -24,7 +31,7 @@ const contactSlice = createSlice({
 export const { addContact, deleteContact } = contactSlice.actions;
 export const selectContacts = state => state.contacts.items;
 
-export default contactSlice.reducers;
+export default contactSlice.reducer;
 
 
 
