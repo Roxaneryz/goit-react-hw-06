6c6d441039250da1aa4cae634c8f0f025baa45b4
Roxaneryz <roxane5666@yahoo.gr>
2024-06-27 12:15:40 +0300

    contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,25 @@
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css"
 import { useSelector, useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contactsSlice";
-
+import { deleteContact, selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
 
 const ContactList = () => {
-  const contacts = useSelector((state) => state.contacts.items);
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
   const dispatch = useDispatch();
+
+// const contacts = useSelector((state) => state.contacts.items);
+  const filteredContacts = contacts.filter((contact) => {
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  });
 
   const handleDelContact = (id) => {
     dispatch(deleteContact(id));
   }
   return (
     <ul className={css.contactList}>
-      {contacts.map(({ id, name, number }) => (
+      {filteredContacts.map(({ id, name, number }) => (
         <li className={css.contactLiItem} key={id}>
           <Contact
             id={id}
